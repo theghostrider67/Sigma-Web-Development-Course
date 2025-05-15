6a50f71699eeb7aca589a5bfa82893f63020034f
GHOST <tasinhossainorko178867@yahoo.com>
2025-05-15 14:03:43 +0600

    Oh

---
+++ V54/script.js
@@ -0,0 +1,15 @@
+alert("Hello Boys");
+
+console.log("The code is running");
+console.log("The code is running joss"); 
+
+var a = prompt("Enter a number")
+var isTrue = confirm("Are you sure that you want to continue? or your potato pc will be blast")
+if (isTrue) {
+    console.log("You are the king of the north")
+}
+else {
+    console.log("You are not the king of the north")
+}
+
+console.log("Your number is: " + a);
