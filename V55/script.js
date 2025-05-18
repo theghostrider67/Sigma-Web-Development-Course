05077a0aee86b021275fd7e9922ea98de57321b2
GHOST <tasinhossainorko178867@yahoo.com>
2025-05-18 19:41:37 +0600

    UPDATE

---
+++ V55/script.js
@@ -0,0 +1,15 @@
+console.log("Hello, World!");
+
+var x = 5;
+var y = 10;
+var z ="GHOST";
+
+// console.log(x+y+69);
+// console.log(z);
+// console.log(typeof x, typeof y, typeof z);
+
+{
+    // let x = 55;
+    console.log(x);
+}
+console.log(x);
