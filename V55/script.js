497ef63fcd4410929330916473fbce33a857db95
GHOST <tasinhossainorko178867@yahoo.com>
2025-05-18 20:02:31 +0600

    A

--- V55/script.js
+++ V55/script.js
@@ -1,8 +1,9 @@
 console.log("Hello, World!");
 
 var x = 5;
-var y = 10;
-var z ="GHOST";
+let y = 10;
+let z ="GHOST";
+let _x = "TASIN";
 
 // console.log(x+y+69);
 // console.log(z);
@@ -13,3 +14,26 @@ var z ="GHOST";
     console.log(x);
 }
 console.log(x);
+// const a = 19;
+// a = a + 2;
+
+let a = "GHOST TASIN";
+let b = 22;
+let c = 3.14;
+const d = true;   
+let e = undefined;
+let f = null;
+
+console.log(a,b,c,d,e,f);
+console.log(typeof a,typeof b,typeof c,typeof d,typeof e,typeof f);
+
+let o = {
+    "Name": "GHOST",
+    "Age": 22, 
+    "isMarried": false
+}
+console.log(o)
+o.Salary = "1000Billion"
+console.log(o)
+o.Salary = "9999Billion"
+console.log(o)
