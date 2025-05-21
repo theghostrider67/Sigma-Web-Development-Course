dc54871b925dca6acc7f704995e1796cfad88d95
GHOST <tasinhossainorko178867@yahoo.com>
2025-05-22 02:52:09 +0600

    Update

--- V56/index.js
+++ V56/index.js
@@ -1,16 +1,16 @@
 console.log("Hello, world! JavaScript Conditionals");
 
 let age = 0;
-// let add = 5
-// age = age + add;
+let add = 5
+age = age + add;
 
-// console.log(age);
-// console.log(age + add);
-// console.log(age - add);
-// console.log(age * add);
-// console.log(age / add);
-// console.log(age ** add);
-// console.log(age % add);
+console.log(age);
+console.log(age + add);
+console.log(age - add);
+console.log(age * add);
+console.log(age / add);
+console.log(age ** add);
+console.log(age % add);
 
 if(age>=18)
 {
@@ -25,46 +25,52 @@ else
     console.log("You are a not an adult");
 }
 
-// if(age>=18)
-// {
-//     console.log("You are an adult");
-// }
-// else
-// {
-//     console.log("You are a not an adult");
-// }
+a = 6;
+b = 8;
+c = a>b ? (a-b):(b-a)
+console.log(c);
 
-// if(age==18)
-// {
-//     console.log("You are an adult");
-// }
-// else
-// {
-//     console.log("You are a not an adult");
-// }
 
-// if(age!=18)
-// {
-//     console.log("You are an adult");
-// }
-// else
-// {
-//     console.log("You are a not an adult");
-// }
-// if(age!=18 && age>0)
-// {
-//     console.log("You are an adult");
-// }
-// else
-// {
-//     console.log("You are a not an adult");
-// }
-// if(age!=18 || age>0)
-// {
-//     console.log("You are an adult");
-// }
-// else
-// {
-//     console.log("You are a not an adult");
-// }
+if(age>=18)
+{
+    console.log("You are an adult");
+}
+else
+{
+    console.log("You are a not an adult");
+}
+
+if(age==18)
+{
+    console.log("You are an adult");
+}
+else
+{
+    console.log("You are a not an adult");
+}
+
+if(age!=18)
+{
+    console.log("You are an adult");
+}
+else
+{
+    console.log("You are a not an adult");
+}
+if(age!=18 && age>0)
+{
+    console.log("You are an adult");
+}
+else
+{
+    console.log("You are a not an adult");
+}
+if(age!=18 || age>0)
+{
+    console.log("You are an adult");
+}
+else
+{
+    console.log("You are a not an adult");
+}
 
