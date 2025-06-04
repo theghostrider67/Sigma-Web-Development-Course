56673d0cbeae6ad90c210eac84bdf6f4e9d1e0c2
GHOST <tasinhossainorko178867@yahoo.com>
2025-06-05 00:21:41 +0600

    New

---
+++ V60/index.js
@@ -0,0 +1,28 @@
+console.log("Hey This a the index.js file of JavaScript in south california"); 
+
+let a = "GHOST";
+
+console.log(a);
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+console.log(a[5]); // undefined
+console.log(a.length);
+
+let realname = "Ghost";
+let age = 21;
+
+console.log("My name is " + realname + " and my age is " + age);
+
+console.log(`My name is ${realname} and my age is ${age}`); 
+
+let b = "RIDERRI";
+console.log(b.toUpperCase());
+console.log(b.toLowerCase());
+console.log(b.length);
+console.log(b.slice(2, 4)); 
+console.log(b.slice(2)); 
+console.log(b.replace ("RI", "69"));
+console.log(b.concat(" is a " ,a));
