90f0639a116df442f2ab4a2ea00a828c75f102ca
GHOST <tasinhossainorko178867@yahoo.com>
2025-08-27 22:34:39 +0600

    Up

--- V70EX12/index.js
+++ V70EX12/index.js
@@ -1,21 +1,5 @@
-const box = document.getElementsByClassName("box");
-for (let i = 0; i < box.length; i++) {
-    box[i].style.backgroundColor = "rgb(100,200,50)";
-    box[i].style.color = "rgba(255, 255, 255, 1)";
+console.log("EXERCISE USING DOM CONCEPTS");
+let boxes = document.getElementsByClassName("box");
+console.log(boxes);
 
-    const rBg = Math.floor(Math.random() * 256);
-    const gBg = Math.floor(Math.random() * 256);
-    const bBg = Math.floor(Math.random() * 256);
-
-    let rText, gText, bText;
-    do {
-        rText = Math.floor(Math.random() * 256);
-        gText = Math.floor(Math.random() * 256);
-        bText = Math.floor(Math.random() * 256);
-    } 
-    while (rText === rBg && gText === gBg && bText === bBg);
-
-    box[i].style.backgroundColor = `rgb(${rBg},${gBg},${bBg})`;
-    box[i].style.color = `rgb(${rText},${gText},${bText})`;
-
-}
+Array.from(boxes).forEach 
